5efed300c85700491348dbc0f4b9e016bde6136e
Artem <artem.shakirov23@mail.ru>
2022-08-10 17:00:16 +0300

    Fix main screen

--- app/src/main/java/com/example/trevelguidfree/MainActivity.kt
+++ app/src/main/java/com/example/trevelguidfree/MainActivity.kt
@@ -9,29 +9,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-        val buttonAttractions: Button = findViewById(R.id.button)
-
-        buttonAttractions.setOnClickListener {
-            val text = Toast.makeText(this, "Seconds... We open 'Attractions'", Toast.LENGTH_SHORT)
-            text.show()
-        }
-
-
-        val buttonNeedToKnow: Button = findViewById(R.id.button1)
-
-        buttonNeedToKnow.setOnClickListener {
-            val text = Toast.makeText(this, "Seconds... We open 'Need To Know'", Toast.LENGTH_SHORT)
-            text.show()
-        }
-
-
-        val buttonMore: Button = findViewById(R.id.button2)
-
-        buttonMore.setOnClickListener {
-            val text = Toast.makeText(this, "Seconds... We open 'More'", Toast.LENGTH_SHORT)
-            text.show()
-        }
     }
 }
